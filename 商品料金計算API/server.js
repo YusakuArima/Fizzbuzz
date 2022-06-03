dc76d85d880f430db8f2b500b061565c9c850ab1
YusakuArima <arima@or-not.com>
2022-06-03 19:50:36 +0900

    Progress(2022/6/3)

--- 商品料金計算API/server.js
+++ 商品料金計算API/server.js
@@ -3,6 +3,7 @@ const { json } = require('express/lib/response');
 const app = express();
 const PORT = 3000;
 
+
 //アプリ内データ
 const arrayObject = [
     { id: 1, productName: 'T-Shirt', price: 4800},
@@ -17,8 +18,8 @@ const productId = params.get('product_id');
 const amount =params.get('amount');
 
 //引数に商品IDを受け取ると、対応した商品データのObjectを返す関数を書く
-
 const items = (productID, amount) => {
+//要修正
     if(productID === undefined){
         console.log('404')
     }else if(amount === undefined){
@@ -31,6 +32,12 @@ const items = (productID, amount) => {
     }
 }
 items(productId,amount);
+
+/*次回：
+下記の実装
+「商品ID、個数どちらかがリクエストに足りない場合、リクエスト時の商品IDに対応する商品がない場合は、レスポンスコード404を返す」
+JSONで返却できるようにする。
+*/
 
 
 app.listen(PORT, () =>console.log('OPEN'));
